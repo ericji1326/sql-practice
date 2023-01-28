1d652f4a10bd5e5ec65fff3e82f708ec58273a86
ericji1326 <ericji1326@gmail.com>
2023-01-28 02:09:42 -0500

    finished last q in medium

--- medium.sql
+++ medium.sql
@@ -188,9 +188,25 @@ select first_name, last_name, count(*) as num_of_duplicates from patients
 group By first_name, last_name
 having num_of_duplicates > 1
 
+-- Display patient's full name,
+-- height in the units feet rounded to 1 decimal,
+-- weight in the unit pounds rounded to 0 decimals,
+-- birth_date,
+-- gender non abbreviated.
 
-
-
+-- Convert CM to feet by dividing by 30.48.
+-- Convert KG to pounds by multiplying by 2.205.
+select 
+(concat(first_name, ' ', last_name)) as patients_name, 
+round(height/30.48,1) as 'height "Feet"',
+ROUND(weight*2.205,0) as 'weight "Pounds"',
+birth_date,
+CASE
+      WHEN gender = 'M' THEN 'MALE'
+      WHEN gender = 'F' THEN 'FEMALE'
+ END AS gender_type
+from
+patients
 
 
 
