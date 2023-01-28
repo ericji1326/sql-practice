7c36a1088615cfbeb3c4352bd79c8b57615b074b
ericji1326 <ericji1326@gmail.com>
2023-01-27 23:50:09 -0500

    removing spaces

--- easy.sql
+++ easy.sql
@@ -82,21 +82,3 @@ where city = 'Hamilton' and allergies not null
 select distinct(city) from patients
 where city like 'a%' or city like 'e%' or city like 'i%' or city like 'o%' or city like 'u%'
 order by city asc
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
